9effa684138bf5ac1f13c69efd7b584c5fee6096
Manuel Dalla Lana <manuel@pepeverde.agency>
2016-11-21 12:20:09 +0100

    Apply PSR-2 coding style

--- src/LukeSnowden/GoogleShoppingFeed/Containers/GoogleShopping.php
+++ src/LukeSnowden/GoogleShoppingFeed/Containers/GoogleShopping.php
@@ -5,36 +5,36 @@ use LukeSnowden\GoogleShoppingFeed\Feed;
 class GoogleShopping
 {
 
-	/**
-	 * [$containers description]
-	 * @var array
-	 */
+    /**
+     * [$containers description]
+     * @var array
+     */
 
-	public static $container = null;
+    public static $container = null;
 
-	/**
-	 * [container description]
-	 * @return [type] [description]
-	 */
+    /**
+     * [container description]
+     * @return [type] [description]
+     */
 
-	public static function container()
-	{
-		if ( is_null( static::$container ) ) {
-			static::$container = new Feed;
-		}
-		return static::$container;
-	}
+    public static function container()
+    {
+        if (is_null(static::$container)) {
+            static::$container = new Feed;
+        }
 
-	/**
-	 * [__callStatic description]
-	 * @param  [type] $name      [description]
-	 * @param  [type] $arguments [description]
-	 * @return [type]            [description]
-	 */
+        return static::$container;
+    }
 
-	public static function __callStatic( $name, $arguments )
-	{
-		return call_user_func_array( array( static::container(), $name ), $arguments );
-	}
+    /**
+     * [__callStatic description]
+     * @param  [type] $name      [description]
+     * @param  [type] $arguments [description]
+     * @return [type]            [description]
+     */
 
+    public static function __callStatic($name, $arguments)
+    {
+        return call_user_func_array(array(static::container(), $name), $arguments);
+    }
 }
--- src/LukeSnowden/GoogleShoppingFeed/Node.php
+++ src/LukeSnowden/GoogleShoppingFeed/Node.php
@@ -1,88 +1,94 @@
 <?php namespace LukeSnowden\GoogleShoppingFeed;
 
-class Node {
+class Node
+{
 
-	/**
-	 * [$name description]
-	 * @var null
-	 */
-	protected $name = null;
+    /**
+     * [$name description]
+     * @var null
+     */
+    protected $name = null;
 
-	/**
-	 * [$namespace description]
-	 * @var null
-	 */
-	protected $_namespace = null;
+    /**
+     * [$namespace description]
+     * @var null
+     */
+    protected $_namespace = null;
 
-	/**
-	 * [$value description]
-	 * @var string
-	 */
-	protected $value = '';
+    /**
+     * [$value description]
+     * @var string
+     */
+    protected $value = '';
 
-	/**
-	 * [$cdata description]
-	 * @var boolean
-	 */
-	protected $cdata = false;
+    /**
+     * [$cdata description]
+     * @var boolean
+     */
+    protected $cdata = false;
 
-	/**
-	 * [__construct description]
-	 * @param [type] $name [description]
-	 */
-	public function __construct( $name ) {
-		$this->name = $name;
-		return $this;
-	}
+    /**
+     * [__construct description]
+     * @param [type] $name [description]
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
 
-	/**
-	 * [namespace description]
-	 * @param  [type] $value [description]
-	 * @return [type]        [description]
-	 */
-	public function _namespace( $value ) {
-		$this->_namespace = $value;
-		return $this;
-	}
+    /**
+     * [namespace description]
+     * @param  [type] $value [description]
+     * @return [type]        [description]
+     */
+    public function _namespace($value)
+    {
+        $this->_namespace = $value;
+        return $this;
+    }
 
-	/**
-	 * [addCdata description]
-	 */
-	public function addCdata() {
-		$this->cdata = true;
-		return $this;
-	}
+    /**
+     * [addCdata description]
+     */
+    public function addCdata()
+    {
+        $this->cdata = true;
+        return $this;
+    }
 
-	/**
-	 * [value description]
-	 * @param  [type] $value [description]
-	 * @return [type]        [description]
-	 */
-	public function value( $value ) {
-		$this->value = $value;
-		return $this;
-	}
+    /**
+     * [value description]
+     * @param  [type] $value [description]
+     * @return [type]        [description]
+     */
+    public function value($value)
+    {
+        $this->value = $value;
+        return $this;
+    }
 
-	/**
-	 * [get description]
-	 * @param  [type] $key [description]
-	 * @return [type]      [description]
-	 */
-	public function get( $key ) {
-		return $this->{$key};
-	}
+    /**
+     * [get description]
+     * @param  [type] $key [description]
+     * @return [type]      [description]
+     */
+    public function get($key)
+    {
+        return $this->{$key};
+    }
 
-	/**
-	 * [toXmlNode description]
-	 * @param  [type] $parent [description]
-	 * @return [type]         [description]
-	 */
-	public function attachNodeTo( $parent ) {
-		if( $this->cdata && ! preg_match( "#^<!\[CDATA#is", $this->value ) ) {
-			$this->value = "<![CDATA[{$this->value}]]>";
-		}
-		$parent->addChild( $this->name, '', $this->_namespace );
-		$parent->{$this->name} = $this->value;
-	}
-
+    /**
+     * [toXmlNode description]
+     * @param  [type] $parent [description]
+     * @return [type]         [description]
+     */
+    public function attachNodeTo($parent)
+    {
+        if ($this->cdata && ! preg_match("#^<!\[CDATA#is", $this->value)) {
+            $this->value = "<![CDATA[{$this->value}]]>";
+        }
+        $parent->addChild($this->name, '', $this->_namespace);
+        $parent->{$this->name} = $this->value;
+    }
 }
